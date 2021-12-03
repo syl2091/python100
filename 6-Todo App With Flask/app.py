273129e9060698c726c7cc584d0b4c4d94636e18
onthroad <164378335@qq.com>
2021-12-03 16:57:18 +0800

    Todo App With Flask

--- 6-Todo App With Flask/app.py
+++ 6-Todo App With Flask/app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
