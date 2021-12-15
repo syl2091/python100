c0c5abc17dc8c38764b2931c9f1d229ae0df4c93
onthroad <164378335@qq.com>
2021-12-15 11:25:13 +0800

    Convert dictionary to python object

---
+++ 21-Convert dictionary to python object/conversion.py
@@ -0,0 +1,10 @@
+# 将字典转换为python对象
+
+class obj(object):
+    def __init__(self, d):
+        for x, y in d.items():
+            setattr(self, x, obj(y) if isinstance(y, dict) else y)
+data = {'a':5,'b':7,'c':{'d':8}}
+ob = obj(data)
+
+print(ob)
